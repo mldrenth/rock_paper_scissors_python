4b1a4c962babea12e1df8468608ceac44e7bc107
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-26 16:30:19 +0200

    Added game logic

--- models/game.py
+++ models/game.py
@@ -1,2 +1,15 @@
 class Game():
-    pass
+    def __init__(self):
+        self._win_conditions = {
+            "rock": "scissors",
+            "scissors": "paper",
+            "paper": "rock"
+        }
+    
+    def play_game(self, player_1, player_2):
+        if player_1.choice == player_2.choice:
+            return None
+        elif player_2.choice == self._win_conditions[player_1.choice]:
+            return player_1
+        else:
+            return player_2
--- tests/game_test.py
+++ tests/game_test.py
@@ -1,3 +1,4 @@
+from types import resolve_bases
 import unittest
 
 from models.game import Game
@@ -5,6 +6,35 @@ from models.player import Player
 
 class TestGame(unittest.TestCase):
     def setUp(self):
-        player_rock = Player("Player1", "rock")
-        player_paper = Player("Player2", "paper")
-        player_scissors = Player("Player3", "scissors")
+        self.player_rock = Player("Player1", "rock")
+        self.player_paper = Player("Player2", "paper")
+        self.player_scissors = Player("Player3", "scissors")
+        self.game = Game()
+    
+    def test_game_rock_beats_scissors(self):
+        result = self.game.play_game(self.player_rock, self.player_scissors)
+        self.assertEqual(self.player_rock, result)
+    
+    def test_game_rock_loses_against_paper(self):
+        result = self.game.play_game(self.player_rock, self.player_paper)
+        self.assertEqual(self.player_paper, result)
+    
+    def test_game_scissors_beats_paper(self):
+        result = self.game.play_game(self.player_scissors, self.player_paper)
+        self.assertEqual(self.player_scissors, result)
+    
+    def test_game_scissors_loses_against_rock(self):
+        result = self.game.play_game(self.player_scissors, self.player_rock)
+        self.assertEqual(self.player_rock, result)
+    
+    def test_game_paper_beats_rock(self):
+        result = self.game.play_game(self.player_paper, self.player_rock)
+        self.assertEqual(self.player_paper, result)
+    
+    def test_game_paper_loses_against_scisors(self):
+        result = self.game.play_game(self.player_paper, self.player_scissors)
+        self.assertEqual(self.player_scissors, result)
+    
+    def test_game_return_none_on_tie(self):
+        result = self.game.play_game(self.player_rock, self.player_rock)
+        self.assertEqual(None, result)
