797e535d60a08eab62d405c575f8ceda23db69bb
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-26 17:01:36 +0200

    Finished MVP

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,11 @@ from models.player import Player
 @app.route('/')
 def index():
     return render_template('index.html', title = "Home")
+
+@app.route('/<choice_1>/<choice_2>')
+def play_round(choice_1,choice_2):
+    player_1 = Player("Player 1", choice_1)
+    player_2 = Player("Player 2", choice_2)
+    game = Game()
+    winner = game.play_game(player_1, player_2)
+    return render_template('result.html', title = "Result", winner = winner, player_1 = player_1, player_2 = player_2)
