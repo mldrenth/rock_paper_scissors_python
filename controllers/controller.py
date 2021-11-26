e92e6b71fb180f3836353e9cd119fba3a4c1bf4a
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-26 20:44:19 +0200

    Functionality to play againt computer implemented

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from models.game import Game
 from models.player import Player
@@ -18,3 +18,16 @@ def play_round(choice_1,choice_2):
     game = Game()
     winner = game.play_game(player_1, player_2)
     return render_template('result.html', title = "Result", winner = winner, player_1 = player_1, player_2 = player_2)
+
+@app.route('/play', methods=["POST"])
+def play_against_computer():
+    game = Game()
+    # Create human player from form
+    player_name = request.form["name"]
+    player_choice = request.form["options"]
+    human_player = Player(player_name, player_choice)
+    # Create computer player
+    computer_player = game.create_computer_player()
+    # Play a game and render
+    winner = game.play_game(human_player, computer_player)
+    return render_template('result.html', title = "Result", winner = winner, player_1 = human_player, player_2 = computer_player)
